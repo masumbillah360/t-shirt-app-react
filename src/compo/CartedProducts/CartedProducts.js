744546c263b77beb658f1cd4d98809d9566dd346
masumbillah360 <masumbillah6584@gmail.com>
2022-10-08 00:48:37 +0600

    upload overall structure and setup router

---
+++ src/compo/CartedProducts/CartedProducts.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CartedProducts = () => {
+    return (
+        <div>
+            <h1>All Carted Products</h1>
+        </div>
+    );
+};
+
+export default CartedProducts;
---
+++ src/compo/TShirt/TShirt.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const TShirt = () => {
+    return (
+        <div>
+            <h1>T-Shirt Page</h1>
+        </div>
+    );
+};
+
+export default TShirt;
